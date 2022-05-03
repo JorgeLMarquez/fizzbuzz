9bcfa19ddf3d77df2e678f0102cf13aa9f3bf59e
Jorge Luis Márquez León <launchx03816@innovaccion.mx>
2022-05-03 17:49:31 -0500

    Corrigiendo indez.js

--- index.js
+++ index.js
@@ -5,18 +5,25 @@ const FizzbuzzService = require("./lib/services/FizzbuzzService");
 const explorers = Reader.readJsonFile("explorers.json");
 
 // Aplicación del ExplorerService sobre la lista de explorers
+
 console.log("\n\tLista de explorers que solo están en node");
 console.log(ExplorerService.filterByMission(explorers, "node"));
+
 console.log("\n\tCantidad de explorers que solo están en node");
 console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"));
+
 console.log("\n\tLista de usuarios de github de los explorers que están en node.");
 console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node"));
+
 console.log("\n\tValidaciones.");
 const explorer1 = {name: "Explorer1", score: 1};
 console.log(FizzbuzzService.applyValidationInExplorer(explorer1));
+
 const explorer3 = {name: "Explorer3", score: 3};
-console.log(FizzbuzzService.applyValidationInExplorer(explorer3)); // {name: "Explorer3", score: 3, trick: "FIZZ"}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer3)); 
+
 const explorer5 = {name: "Explorer5", score: 5};
-console.log(FizzbuzzService.applyValidationInExplorer(explorer5)); // {name: "Explorer5", score: 5, trick: "BUZZ"}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer5)); 
+
 const explorer15 = {name: "Explorer15", score: 15};
-console.log(FizzbuzzService.applyValidationInExplorer(explorer15)); // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer15)); 
