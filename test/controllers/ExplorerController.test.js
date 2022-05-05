796add1f088cf862b71d7fefc06cf7bbc1ed9e5a
Jorge Luis Márquez León <launchx03816@innovaccion.mx>
2022-05-05 14:42:27 -0500

    Agregando tests

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -4,17 +4,22 @@ describe("Test for ExplorerController",()=> {
 
     test("1.Get explorers by mission",()=>{
         const explorers = ExplorerController.getExplorersByMission("node");
-        expect(explorers.length).toBe(10);        
+        expect(explorers.length).toBe(10);
     });
 
     test("2. Get explorers usernames by mission",()=>{
         const explorers = ExplorerController.getExplorersUsernamesByMission("node");
-        expect(explorers[0]).toBe("ajolonauta1");        
-    });    
+        expect(explorers[0]).toBe("ajolonauta1");
+    });
 
     test("3. Get explorers amount by mission",()=>{
         const explorers = ExplorerController.getExplorersAmountByMission("node");
-        expect(explorers).toBe(10);        
+        expect(explorers).toBe(10);
     });
-    
- });
+
+    test("4. Fizzbuzz Validation",()=>{
+        const fizzbuzz = ExplorerController.getFizzbuzzValidation(3);
+        expect(fizzbuzz).toBe("FIZZ");        
+    });
+
+});
--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -3,20 +3,31 @@ const FizzbuzzService = require("../../lib/services/FizzbuzzService");
 const Reader = require("./../../lib/utils/Reader");
 
 
-describe("Test para FizzbuzzService", () =>{ 
+describe("Test para FizzbuzzService", () =>{
     const explorers = Reader.readJsonFile("./explorers.json");
-      
-    test("Requerimiento 3: Lista de usuarios de github de los explorers que están en node.", () => {
+
+    test("Requerimiento 3: Fizzbuzz trick", () => {
         const explorer1 = {name: "Explorer1", score: 1};
         const explorer3 = {name: "Explorer3", score: 3};
         const explorer5 = {name: "Explorer5", score: 5};
         const explorer15 = {name: "Explorer15", score: 15};
-				
+
         expect(FizzbuzzService.applyValidationInExplorer(explorer1)).toMatchObject({name: "Explorer1", score: 1, trick: 1});
         expect(FizzbuzzService.applyValidationInExplorer(explorer3)).toMatchObject({name: "Explorer3", score: 3, trick: "FIZZ"});
         expect(FizzbuzzService.applyValidationInExplorer(explorer5)).toMatchObject({name: "Explorer5", score: 5, trick: "BUZZ"});
         expect(FizzbuzzService.applyValidationInExplorer(explorer15)).toMatchObject({name: "Explorer15", score: 15, trick: "FIZZBUZZ"});
     });
-    
-    
+
+    test("Requerimiento 3.1: Fizzbuzz trick API", () => {
+        const number1 = 1;
+        const number3 = 3;
+        const number5 = 5;
+        const number15 = 15;
+
+        expect(FizzbuzzService.applyValidationInNumber(number1)).toBe(1);
+        expect(FizzbuzzService.applyValidationInNumber(number3)).toBe("FIZZ");
+        expect(FizzbuzzService.applyValidationInNumber(number5)).toBe("BUZZ");
+        expect(FizzbuzzService.applyValidationInNumber(number15)).toBe("FIZZBUZZ");
+    });
+
 });
